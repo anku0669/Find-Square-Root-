638b723af2eb87a8c5bb02e8f686485cc74414af
anku0669 <159039485+anku0669@users.noreply.github.com>
2024-11-10 10:03:06 +0530

    Add files via upload

---
+++ BIsectionMethod.java
@@ -0,0 +1,35 @@
+public class BIsectionMethod {
+    public BIsectionMethod() {
+        super();
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        super.finalize();
+    }
+    double number = 45566767;
+
+    double squareroot =Math.sqrt(number);
+
+}
+
---
+++ mainn.java
@@ -0,0 +1,15 @@
+public class mainn {
+
+
+        public static void main(String[] args) {
+
+            double number;
+            number = 1000000000;
+
+
+            double squareRoot = Math.sqrt(number);
+
+
+            System.out.println("The square root of " + number + " is: " + squareRoot);
+        }
+    }
